4cd5bc8adafeb4bed167d2626af0cdc6ab844823
Kundank7 <149052665+Kundank7@users.noreply.github.com>
2025-04-07 16:11:59 +0530

    app.py

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, send_file, render_template
+from rembg import remove
+from PIL import Image
+import io
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/remove-bg', methods=['POST'])
+def remove_bg():
+    if 'image' not in request.files:
+        return 'No image uploaded!', 400
+
+    file = request.files['image']
+    img = Image.open(file.stream)
+    output = remove(img)
+
+    img_byte_arr = io.BytesIO()
+    output.save(img_byte_arr, format='PNG')
+    img_byte_arr.seek(0)
+
+    return send_file(img_byte_arr, mimetype='image/png')
+
+if __name__ == '__main__':
+    app.run(debug=True)
